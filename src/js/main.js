3fb60e979347335aabd281f64226e72379bd3858
Dein Name <dein.name@example.com>
2025-04-01 13:50:29 +0200

    Steuerung: Shift-Taste durch Strg-Taste für Schub ersetzt

--- src/js/main.js
+++ src/js/main.js
@@ -399,7 +399,7 @@ let levelDesigner;
 const keys = {
     a: false,
     s: false,
-    shift: false,
+    control: false,
     enter: false,
     space: false
 };
@@ -550,7 +550,7 @@ function handleKeyDown(event) {
     switch(event.key.toLowerCase()) {
         case 'a': keys.a = true; break;
         case 's': keys.s = true; break;
-        case 'shift': keys.shift = true; break;
+        case 'control': case 'ctrl': keys.control = true; break;  // Beide Varianten unterstützen
         case 'enter': keys.enter = true; break;
         case ' ': keys.space = true; break;
     }
@@ -560,7 +560,7 @@ function handleKeyUp(event) {
     switch(event.key.toLowerCase()) {
         case 'a': keys.a = false; break;
         case 's': keys.s = false; break;
-        case 'shift': keys.shift = false; break;
+        case 'control': case 'ctrl': keys.control = false; break;  // Beide Varianten unterstützen
         case 'enter': keys.enter = false; break;
         case ' ': keys.space = false; break;
     }
@@ -574,7 +574,7 @@ function animate() {
     if (!gameState.isGameOver) {
         if (keys.a) spaceship.rotateLeft();
         if (keys.s) spaceship.rotateRight();
-        spaceship.thrust = keys.shift;
+        spaceship.thrust = keys.control;
         
         if (keys.space) {
             spaceship.activateShield();
